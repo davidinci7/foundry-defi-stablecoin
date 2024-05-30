9de1e0b6ee1c30fe26bea57bee2da28600d340f2
David R <davidalexrodriguez@hotmail.com>
2024-05-30 16:55:22 -0600

    Create first version of DeployDSC and HelperConfig scripts

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+
+pragma solididty ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+
+contract HelperConfig is Script{
+    struct NetworkConfig {
+        address wethUsdPriceFeed;
+        address wbtcUsdPriceFeed;
+        address weth;
+        address wbtc;
+        uint256 deployerKey;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    function getSepoliaEthConfig() public view returns(NetworkConfig memory){
+        
+    }
+}
